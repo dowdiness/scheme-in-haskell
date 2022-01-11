70c3d61b3759d18387e161afbe4c0ac2253def3b
Koji Ishimoto <koji.ishimoto@gmail.com>
2022-01-12 02:09:19 +0900

    add Repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
-import           Lib
-import           System.Environment
+import           Repl               (evalAndPrint, runRepl)
+import           System.Environment (getArgs)
 
 main :: IO ()
 main = do
   args <- getArgs
-  let evaled = fmap show $ readExpr (head args) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ head args
+    _ -> putStrLn "Program takes only 0 or 1 argument"
---
+++ src/Repl.hs
@@ -0,0 +1,26 @@
+module Repl where
+
+import           Lib       (eval, extractValue, readExpr, trapError)
+import           System.IO (hFlush, stdout)
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (fmap show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt ">>") evalAndPrint
